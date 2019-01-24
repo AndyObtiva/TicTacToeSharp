098a3e37de1f20b70e8ddb7b7fbd7d70b24956b0
Andy Maleh <andy.am@gmail.com>
2019-01-23 20:25:19 -0600

    Initial Tic Tac Toe project solution files

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O X X");
+            Console.WriteLine("X O X");
+            Console.WriteLine("X X O");
+        }
+    }
+}
